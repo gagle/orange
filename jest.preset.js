c2004481cca8a56fc35d73e4c99e66aba90523f1
Gabriel Llamas <gabriel.llamas.llopis@gmail.com>
2021-11-27 00:23:24 +0100

    config jest

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,22 @@
 const nxPreset = require('@nrwl/jest/preset');
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  clearMocks: true,
+  collectCoverage: true,
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'environment.*'
+  ],
+  coverageReporters: ['html', 'cobertura', 'lcov', 'text'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  reporters: ['default'],
+  restoreMocks: true
+};
